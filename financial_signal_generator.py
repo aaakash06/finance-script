f267d40eec140a1dfdba53e6e41575d64c07453a
aaakash06 <akashbagale182@gmail.com>
2024-12-17 00:08:32 +0545

    loadenv

--- financial_signal_generator.py
+++ financial_signal_generator.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class FinancialSignalGenerator:
     def __init__(self):
-        pass
+        self.api_key = os.getenv("FMP_API_KEY")
 
     def generate_signals(self):
         pass
